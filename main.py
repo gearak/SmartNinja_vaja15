d98428601f0a922e85e07ab960970991b79a7ce7
Gea Rak <rak.gea@gmail.com>
2019-05-30 18:12:55 +0200

    Vaja16

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template
-
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    some_text = 'Message from handler!'
+    current_year = datetime.datetime.now().year
+    cities = ["Boston", "Ljubljana", "Dunaj", "Beograd"]
+    logged_in = True
+    return render_template('index.html', some_text=some_text, current_year=current_year, cities=cities, logged_in=logged_in)
 
 @app.route('/about_me')
 def about_me():
@@ -15,4 +19,4 @@ def portfolio():
     return render_template('portfolio.html')
 
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    app.run()
